62d239bf50dd33358df92d30e8aaa3a5fca854f7
Christine <yang.christine.95@gmail.com>
2021-03-08 19:06:13 -0500

    Amend

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         test: [/\.jsx?$/],
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
+        options: {
           presets: ['es2015', 'react']
         }
       }
