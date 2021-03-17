d7778c5febc3135a39d459b002030422c39faf55
Christine <yang.christine.95@gmail.com>
2021-03-17 01:14:26 -0400

    Fixed front and back auth

--- frontend/components/nav_bar/navbar.jsx
+++ frontend/components/nav_bar/navbar.jsx
@@ -2,35 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ currentUser, logout }) => {
-    const navBar = () => (
-        <nav className="nav-bar">
+    const navLeft = 
+        <span className="nav-left">
             <Link to="/" >
                 <img src={window.logoURL}/>
                 <h1 className="nav-logo">Pintage</h1>
             </Link>
-            <span className="nav-right">
-                <Link className ="nav-login" to='/login'>Log In</Link>
-                <Link className ="nav-signup" to='/signup'>Sign Up</Link>
-            </span>
-        </nav>
-    );
-    
-    const navBarUser = () => (
-        <nav className="nav-bar">
-            <Link to="/" >
-                <img src={window.logoURL}/>
-                <h1 className="nav-logo">Pintage</h1>
-            </Link>
+        </span>;
+        
+    let navRight;
+    if (currentUser) {
+        navRight = 
             <span className="nav-right">
                 <button className="header-button">Add Toggle Here</button>
                 <div className="header-button-container">
                     <button onClick={logout}>Log Out</button> 
                 </div>
             </span>
-        </nav>
-    )
+    } else {
+        navRight = 
+            <span className="nav-right">
+                <Link className ="nav-login" to='/login'>Log In</Link>
+                <Link className ="nav-signup" to='/signup'>Sign Up</Link>
+            </span>
+    }
 
-    return currentUser ? navBar() : navBarUser();
+    return (
+        <nav className="nav-bar">
+            {navLeft}
+            {navRight}
+        </nav>
+    );
+
 }
 
 export default Navbar;
