8c796deb61c46931eebcaf3c044402a7fbeff8e1
Christine <yang.christine.95@gmail.com>
2021-03-19 14:14:26 -0400

    Fix

--- frontend/components/landing/landing.jsx
+++ frontend/components/landing/landing.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div className="landing-header">
-            <div className="landing-title">
-                <h1>Get your next</h1>
-                <ul className="idea-module">
-                    <li>food idea</li>
-                    <li>home idea</li>
-                    <li>travel idea</li>
-                </ul>
+        <div className="landing">
+            <div className="landing-header">
+                <div className="landing-title">
+                    <h1>Get your next</h1>
+                    <ul className="idea-module">
+                        {/* <li>food idea</li>
+                        <li>home idea</li> */}
+                        <li>travel idea</li>
+                    </ul>
+                </div>
+                <div className="landing-imgs">
+                    <div className="one"></div>
+                    <div className="two"></div>
+                    <div className="three"></div>
+                    <div className="four"></div>
+                    <div className="five"></div>
+                    <div className="six"></div>
+                    <div className="seven"></div>
+                    <div className="eight"></div>
+                    <div className="nine"></div>
+                </div>
             </div>
-            <div className="landing-imgs">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>9</div>
-                <div>10</div>
-            </div>
+            <div className="fade">FADE</div>
         </div>
     )
 }
