f6efbc29ea28d009bf963c4cc2805d01b79fffd8
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-06-16 15:53:39 -0400

    Add some seeded files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
-User.destroy_all
-Pin.destroy_all
-Board.destroy_all
-ApplicationRecord.connection.reset_pk_sequence!('users')
-ApplicationRecord.connection.reset_pk_sequence!('pins')
-ApplicationRecord.connection.reset_pk_sequence!('boards')
+# User.destroy_all
+# Pin.destroy_all
+# Board.destroy_all
+# ApplicationRecord.connection.reset_pk_sequence!('users')
+# ApplicationRecord.connection.reset_pk_sequence!('pins')
+# ApplicationRecord.connection.reset_pk_sequence!('boards')
 
-users = User.create([
-    {username: "DemoUser",  password: "hunter10", email: "test@gmail.com"},
-    {username: "test1", password: "hunter11", email: "test1@gmail.com"},
-    {username: "test2", password: "hunter12", email: "test2@gmail.com"}
-])
+# users = User.create([
+#     {username: "DemoUser",  password: "hunter10", email: "test@gmail.com"},
+#     {username: "test1", password: "hunter11", email: "test1@gmail.com"},
+#     {username: "test2", password: "hunter12", email: "test2@gmail.com"}
+# ])
 
-b = Board.create(name: "Saved", user_id: 1)
-b1 = Board.create(name: "Travel", user_id: 1)
+# b = Board.create(name: "Saved", user_id: 1)
+# b1 = Board.create(name: "Travel", user_id: 1)
 
-p1 = Pin.create!(title: "Japanese Night Walk", about: "Nice night", user_id: 1, board_id: 1)
-file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel.jpg")
-p1.photo.attach(io: file, filename: "travel.jpg")
+# p1 = Pin.create!(title: "Japanese Night Walk", about: "Nice night", user_id: 1, board_id: 1)
+# file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel.jpg")
+# p1.photo.attach(io: file, filename: "travel.jpg")
 
-p2 = Pin.create!(title: "Japan Alter", about: "Sights", user_id: 1, board_id: 1)
-file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel1.jpg")
-p2.photo.attach(io: file, filename: "travel1.jpg")
+# p2 = Pin.create!(title: "Japan Alter", about: "Sights", user_id: 1, board_id: 1)
+# file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel1.jpg")
+# p2.photo.attach(io: file, filename: "travel1.jpg")
 
-p3 = Pin.create!(title: "Hills", about: "Over the mountain and through the woods", user_id: 1, board_id: 2)
-file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel12.jpg")
-p3.photo.attach(io: file, filename: "travel12.jpg")
+# p3 = Pin.create!(title: "Hills", about: "Over the mountain and through the woods", user_id: 1, board_id: 2)
+# file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel12.jpg")
+# p3.photo.attach(io: file, filename: "travel12.jpg")
 
 p4 = Pin.create(title: "Japan Day Walk", about: "Sights", user_id: 1)
 file = URI.open("https://pin-tage-seeds.s3.amazonaws.com/travel2.jpg")
