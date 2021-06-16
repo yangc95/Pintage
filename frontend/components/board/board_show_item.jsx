0465185959cf6985af9be84be02e4a1607e4d026
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-06-16 18:22:26 -0400

    Remove aws

--- frontend/components/board/board_show_item.jsx
+++ frontend/components/board/board_show_item.jsx
@@ -5,6 +5,7 @@ export default ({ pinArray }) => {
         return (
         <div className="board-show-pin-index" key={pin.id}>
             <div className="pin-index-image"></div>
+            {/* <img className="pin-index-photo" src={pin.photoUrl} onClick={() => handlePinShow(pin.id)}/> */}
             <h2 className="pin-index-title">{pin.title}</h2>
             <p className="pin-index-about">{pin.about}</p>
         </div>
